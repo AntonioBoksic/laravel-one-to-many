5ef64ffd33b36ceec4806d9c8a076cee26b2f15a
Antonio <antonioboksic1603@gmail.com>
2023-07-25 19:25:09 +0200

    Popolato anche tabella projects. Database completato.

--- laravel-project/database/seeders/DatabaseSeeder.php
+++ laravel-project/database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this -> call([
-            TypeTableSeeder :: class
-            //ProjectTableSeeder :: class,
+            TypeTableSeeder :: class,
+            ProjectTableSeeder :: class,
             
         ]);
     }
